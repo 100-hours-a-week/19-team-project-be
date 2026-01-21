646f4fabb6db47d3ee9abffa89b1181331f6d399
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2026-01-21 11:46:05 +0900

    fix: Swagger, Security Conifg 수정 (#16)
    * feat: 회원 조회 관련 기능 추
    
    * feat: 마스터 테이블 조회 기능 추가
    - 직무, 경력은 테이블 전체 조회
    - 기술은 파라미터에 따라 조건 검
    
    * chore: cors, prod 설정
    
    * chore: jwt 관련 의존성 및 yml 설정
    
    * fix: EnableConfigurationProperties Config설정
    
    * fix: Swagger, Security Config 수정 #15
    
    * fix: conflict fix

--- refit-backend/src/main/java/org/refit/refitbackend/global/config/SecurityConfig.java
+++ refit-backend/src/main/java/org/refit/refitbackend/global/config/SecurityConfig.java
@@ -8,27 +8,39 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public List<String> allowUrls() {
+        return List.of(
+                "/",
+                "/index.html",
+                "/callback.html",
+                "/api/v1/auth/**",
+
+                "/ws/**",
+
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/v3/api-docs/**",
+                "/v3/api-docs/swagger-config"
+        );
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource, List<String> allowUrls) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",
-                                "/index.html",
-                                "/callback.html",
-                                "/api/v1/auth/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        ).permitAll()
+                        .requestMatchers(allowUrls.toArray(new String[0])).permitAll()
                         .anyRequest().permitAll() // TODO: JWT 구현 후 authenticated()로 변경
                 );
 
