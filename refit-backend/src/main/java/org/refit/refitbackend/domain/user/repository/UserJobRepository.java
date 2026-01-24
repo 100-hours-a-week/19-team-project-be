a34d45011640e9e58a2d93f82edea39fe466839e
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2026-01-24 22:45:24 +0900

    feat: 직무/스킬 조회 보완

--- refit-backend/src/main/java/org/refit/refitbackend/domain/user/repository/UserJobRepository.java
+++ refit-backend/src/main/java/org/refit/refitbackend/domain/user/repository/UserJobRepository.java
@@ -2,8 +2,18 @@ package org.refit.refitbackend.domain.user.repository;
 
 import org.refit.refitbackend.domain.user.entity.UserJob;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserJobRepository extends JpaRepository<UserJob, Integer> {
+    @Query("""
+      SELECT uj FROM UserJob uj
+      JOIN FETCH uj.job
+      WHERE uj.user.id IN :userIds
+  """)
+    List<UserJob> findAllByUserIdIn(@Param("userIds") List<Long> userIds);
 }
